a91280b38abd19f00f135e72bea9a285b4c467fa
Vincent Puydoyeux <v.puydoyeux@gmail.com>
2012-11-19 17:58:41 +0100

    HTTPServer v0.1

--- src/ThreadClient.java
+++ src/ThreadClient.java
@@ -1,59 +1,130 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class ThreadClient  extends Thread {
-	private Socket socket;
+	Socket socket;
 	PrintStream out;
+	Scanner in;
 	BufferedReader reader;
-	String line;
-	
-	// Constructeur premenant en paramètre une socket cliente
+	StringTokenizer st;
+	String line, token;
+	Hashtable<String, String> data;
+	/*
+	 *  Constructeur premenant en paramètre une socket cliente
+	 *  Définition des flux input/output en fonction de la socket cliente
+	 */
 	public ThreadClient(Socket s) {
 		socket = s;
+		try {
+			out = new PrintStream(socket.getOutputStream());
+			in = new Scanner(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+			data = new Hashtable<String, String>();
+		} catch (Exception e) {
+			System.out.println("erreur :" + e.getMessage());
+		}
 		this.start();
 	}
-	
+
 	// Surcharge de la méthode run : Redéfinition du flux de sortie pour la socket client
 	public void run() {
-		httpdServerV2();
+		httpdServerV3();
+		
+		try {
+			socket.close();
+		}
+		catch (Exception e) {
+			System.out.println("erreur : " + e.getMessage());
+		}
 	}
-	
+
 	// PART1 : Affiche du contenu HTML brut
 	public void httpdServerV1() {
 		try {
-			out = new PrintStream(socket.getOutputStream());
 			out.println("<HTML>Exemple of HTML file with <BLINK>blinking content</BLINK></HTML>");
 			socket.close();
 		} catch (IOException e) {
 			out.println("erreur: " + e.getMessage());
 		}
 	}
-	
+
 	/*
 	 * PART 2 : Affiche le contenu HTML brut du fichier index.html
 	 * situé dans le répertoire courante du serveur
 	 */
 	public void httpdServerV2() {
 		try {
-			out = new PrintStream(socket.getOutputStream());
 			FileReader file = new FileReader("index.html");
-			reader = new BufferedReader(new FileReader("index.html"));
-			
+			reader = new BufferedReader(file);
+
 			while ((line = reader.readLine()) != null)
 				out.println(line);
-			
+
 			file.close();
 			socket.close();
 		} catch (IOException e) {
 			out.println("erreur: " + e.getMessage());
 		}
 	}
+
+	/*
+	 * PART 3 : Affiche le contenu d'un fichier HTML demandé
+	 * Si le fichier n'existe pas, on affiche par défaut "index.html".
+	 * Si "index.html" n'existe pas on affiche une erreur 404 File Not Found
+	 */
+	public void httpdServerV3() {	
 	
-	public void httpdServerV3() {
-		
+		while (!(line = in.nextLine()).equals("")) {
+			st = new StringTokenizer(line);		
+			while (st.hasMoreTokens()) {
+				token = st.nextToken();
+
+				if (token.equals("GET") && st.hasMoreTokens()) {
+					data.put("METHOD", token);
+					data.put("FILE", st.nextToken());
+				}
+			}
+		}
+
+		if (data.containsKey("METHOD") && data.get("METHOD").equals("GET") && data.containsKey("FILE")) {
+			try {
+				File f = new File(data.get("FILE").replace("/", ""));		
+				if (!f.exists())
+					f = new File("index.html");
+
+				if (f.exists()) {
+					FileReader file = new FileReader(f);
+					reader = new BufferedReader(file);
+
+					out.println("HTTP/1.0 200 OK");
+					out.println("Content-Type: text/HTML");
+					out.println("");
+					while ((line = reader.readLine()) != null && !line.isEmpty())
+						out.println(line);
+					out.println("");
+					
+					file.close();
+				}
+				else {
+					out.println("HTTP/1.0 404");
+					out.println("Content-Type: text/PLAIN");
+					out.println("");
+					out.println("File Not Found");
+					out.println("");
+				}	
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("serreur : " + e.getMessage());
+			}
+		}
 	}
+	
 }
